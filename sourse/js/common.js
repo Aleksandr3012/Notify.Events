cc0b5a709c341e771288ec8bac505edea076d07d
Aleksandr3012 <flaming541@gmail.com>
2020-11-24 20:17:16 +0200

    linksBlock

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -416,6 +416,53 @@ function eventHandler() {
 	$('.accardionBlock__toggle--js').on('click', function(){
 		$(this).parent().find('.accardionBlock__hiddenBlock').slideToggle();
 	})
+
+	var lastId,
+		linksBlock = $(".linksBlock--js"),
+		linksBlockHeight = 20,
+		topMenuHeight = linksBlock.height(),
+		// linksBlockHeight = linksBlock.outerHeight()+15,
+		// All list items
+		menuItems = linksBlock.find("a"),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function () {
+			var item = $($(this).attr("href"));
+			// console.log(item);
+			if (item.length) { return item; }
+		});
+		// console.log(scrollItems);
+		menuItems.click(function (e) {
+			var href = $(this).attr("href"),
+				offsetTop = href === "#" ? 0 : $(href).offset().top;
+			$('html, body').stop().animate({
+				scrollTop: offsetTop
+			}, 1100);
+			e.preventDefault();
+		});
+	
+		// Bind to scroll
+		$(window).scroll(function () {
+			// Get container scroll position
+			var fromTop = $(this).scrollTop();
+
+			// Get id of current scroll item
+			var cur = scrollItems.map(function () {
+				if ($(this).offset().top < fromTop)
+				return this;
+				
+			});
+			// Get the id of the current element
+			cur = cur[cur.length - 1];
+			var id = cur && cur.length ? cur[0].id : "";
+			if (lastId !== id) {
+				lastId = id;
+				// Set/remove active class
+				menuItems
+					.removeClass("active").parent()
+					.end().filter("[href='#" + id + "']").addClass("active");
+			}
+		});
+		
 	
 };
 if (document.readyState !== 'loading') {
